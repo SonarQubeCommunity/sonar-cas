4a11aaff495f36df606b535ba6e9604c584de803
Fabien Crespel <fabien@crespel.net>
2014-07-27 20:05:38 +0200

    Fix RoR CAS controller

--- src/main/resources/org/sonar/ror/cas/app/controllers/cas_controller.rb
+++ src/main/resources/org/sonar/ror/cas/app/controllers/cas_controller.rb
@@ -6,7 +6,7 @@ class CasController < ApplicationController
     # For this reason it's not possible to use /sessions/login
     begin
       login = nil
-      assertion = servlet_request.getAttribute(org.jasig.cas.client.util.AbstractCasFilter::CONST_CAS_ASSERTION)
+      assertion = servlet_request.getAttribute("_const_cas_assertion_")
       unless assertion.nil? || assertion.getPrincipal().nil?
         login = assertion.getPrincipal().getName()
       end
@@ -14,7 +14,7 @@ class CasController < ApplicationController
 
     rescue Exception => e
       self.current_user = nil
-      puts e
+      logger.error(e)
     end
     redirect_back_or_default(home_url)
   end
